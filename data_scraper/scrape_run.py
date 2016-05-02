989d500ca9a8cafedb00ae9f508ca2840c731794
DieterJoubert <dieterjoubert@gmail.com>
2016-05-02 15:26:20 -0700

    update scrape_run

--- data_scraper/scrape_run.py
+++ data_scraper/scrape_run.py
@@ -1,9 +1,9 @@
 def main():
    import sports_scraper as s
 
-   #s.scrape("baseball","http://www.ucsdtritons.com/SportSelect.dbml?&DB_OEM_ID=5800&SPID=2331&SPSID=29807")
+   s.scrape("baseball","http://www.ucsdtritons.com/SportSelect.dbml?SPSID=29807&SPID=2331&DB_OEM_ID=5800&Q_SEASON=2014",False)
 
-   s.scrape("softball","http://www.ucsdtritons.com/SportSelect.dbml?SPSID=29937&SPID=2342&DB_OEM_ID=5800&Q_SEASON=2014",False)
+   #s.scrape("softball","http://www.ucsdtritons.com/SportSelect.dbml?SPSID=29937&SPID=2342&DB_OEM_ID=5800&Q_SEASON=2014",False)
 
    #s.scrape("men_basketball","http://www.ucsdtritons.com/SportSelect.dbml?SPSID=29886&SPID=2337&DB_OEM_ID=5800&Q_SEASON=2014")
 
@@ -12,6 +12,10 @@ def main():
    #s.scrape("men_volleyball","http://www.ucsdtritons.com/SportSelect.dbml?SPSID=29875&SPID=2336&DB_OEM_ID=5800&Q_SEASON=2014",False)
 
    #s.scrape("women_volleyball","http://www.ucsdtritons.com/SportSelect.dbml?SPSID=31944&SPID=2334&DB_OEM_ID=5800&Q_SEASON=2012")
+
+   #s.scrape("men_soccer","http://www.ucsdtritons.com/SportSelect.dbml?SPSID=29740&SPID=2328&DB_OEM_ID=5800&Q_SEASON=2012",False)
+
+   #s.scrape("women_soccer","http://www.ucsdtritons.com/SportSelect.dbml?SPSID=29909&SPID=2339&DB_OEM_ID=5800&Q_SEASON=2015",False)
 
 if __name__ == "__main__":
    main()
